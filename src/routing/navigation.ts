d91e51822f9b8e0220eaf1cc831951b1ba1ae920
justiandevs <justiandev@gmail.com>
2023-11-08 21:22:29 +0100

    Try: create a translation based localized pathname system

--- src/routing/navigation.ts
+++ src/routing/navigation.ts
@@ -3,8 +3,37 @@ import {
   Pathnames,
 } from 'next-intl/navigation'
 import { getAllAvailableLocales } from '@/utils/locale'
+import { createTranslator } from 'next-intl'
 
 export const locales = getAllAvailableLocales()
+
+// TODO: let routing based on routing translations work
+// const getRoutingObjectBasedOnLocale = async (locale: string) => {
+//   const routingTranslation = await import(`../../translations/${locale}.json`)
+//
+//   const t = createTranslator({
+//     messages: routingTranslation,
+//     locale,
+//   })
+//
+//   return routingTranslation.routing
+// }
+//
+// const getPathForLocale = (pathname: string) => {
+//   const pathPerLocale: {
+//     [key: string]: string
+//   } = {}
+//
+//   getAllAvailableLocales().map(async (locale) => {
+//     const routingObject = await getRoutingObjectBasedOnLocale(locale)
+//
+//     // console.log(routingObject, locale, pathname.replace('/', ''))
+//
+//     pathPerLocale[locale] = routingObject[pathname.replace('/', '')]
+//   })
+//
+//   return pathPerLocale
+// }
 
 export const pathnames = {
   '/': '/',
