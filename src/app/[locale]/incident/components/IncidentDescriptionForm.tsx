a00f54bd70cfdad9270d86ae2e5a82dc160691d6
justiandevs <justiandev@gmail.com>
2023-11-13 12:03:37 +0100

    Finalization form footer component

--- src/app/[locale]/incident/components/IncidentDescriptionForm.tsx
+++ src/app/[locale]/incident/components/IncidentDescriptionForm.tsx
@@ -79,11 +79,17 @@ export const IncidentDescriptionForm = () => {
             </FormItem>
           )}
         />
-        <IncidentFormFooter>
+        <IncidentFormFooter className="flex justify-end">
           <Button variant="primary" type="submit">
             {t('describe_submit')}
           </Button>
         </IncidentFormFooter>
+        <p className="text-light_text">
+          Lukt het niet om een melding te doen? Bel het telefoonnummer 14 020
+          <br />
+          Wij zijn bereikbaar van maandag tot en met vrijdag van 08.00 tot 18.00
+          uur.
+        </p>
       </form>
     </Form>
   )
--- src/app/[locale]/incident/components/IncidentFormFooter.tsx
+++ src/app/[locale]/incident/components/IncidentFormFooter.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { cn } from '@/lib/utils/style'
 
 type IncidentFormFooterProps = {} & React.HTMLAttributes<HTMLDivElement>
 
-const IncidentFormFooter = ({ children }: IncidentFormFooterProps) => {
-  return <div className="bg-gray-200 w-full p-3">{children}</div>
+const IncidentFormFooter = ({
+  children,
+  className,
+}: IncidentFormFooterProps) => {
+  return (
+    <div className={cn('bg-gray-200 w-full p-4', className)}>{children}</div>
+  )
 }
 
 export { IncidentFormFooter }
