5892f71ffd7aaf781f36c211456b285835d2811b
justiandevs <justiandev@gmail.com>
2023-11-15 18:38:15 +0100

    Add: Timezone fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,7 @@
 import { getRequestConfig } from 'next-intl/server'
+import { getTimeZone } from '@/lib/utils/locale'
 
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`../translations/${locale}.json`)).default,
+  timeZone: 'Europe/Amsterdam',
 }))
