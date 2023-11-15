0486208c9c2e3f2bd2e10f5bf849123360c5d6a8
justiandevs <justiandev@gmail.com>
2023-11-15 18:39:34 +0100

    Remove: old function getTimezone import

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,4 @@
 import { getRequestConfig } from 'next-intl/server'
-import { getTimeZone } from '@/lib/utils/locale'
 
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`../translations/${locale}.json`)).default,
