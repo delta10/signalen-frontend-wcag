ffef63a45db61fb6c87ef15a44dbea9e58ee34de
justiandevs <justiandev@gmail.com>
2023-11-20 13:40:52 +0100

    Add: Correct styling based on design tokens NLDesignSystem to Checkbox component

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -13,7 +13,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      'peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+      'peer h-6 w-6 shrink-0 ring-1 ring-border transition duration-100 hover:bg-hover hover:ring-2 hover:ring-border focus:ring-2 focus:ring-focus outline-none focus-visible:outline-dashed focus-visible:outline-focus_visible data-[state=checked]:bg-primary data-[state=checked]:text-white data-[state=checked]:active:bg-active data-[state=checked]:hover:bg-hover_link data-[state=checked]:focus:bg-white data-[state=checked]:focus:text-black data-[state=checked]:focus-visible:bg-white',
       className
     )}
     {...props}
