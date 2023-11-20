2095fd5a125ed8a05ef99991449f7daa2fbe9db1
justiandevs <justiandev@gmail.com>
2023-11-20 16:13:00 +0100

    Typo: Fix typo and translated thankyou page

--- src/app/[locale]/incident/thankyou/page.tsx
+++ src/app/[locale]/incident/thankyou/page.tsx
@@ -1,3 +1,19 @@
+import { useTranslations } from 'next-intl'
+
 export default function Thankyou() {
-  return <h1>Thankyou!</h1>
+  const t = useTranslations('describe-thankyou')
+
+  return (
+    <div className="flex flex-col gap-8">
+      <h1>{t('heading')}</h1>
+      <div className="flex flex-col gap-2">
+        <p>{t('description_notification_number')}</p>
+        <p>{t('description_notification_email')}</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <h3>{t('what_do_we_do_heading')}</h3>
+        <p>{t('what_do_we_do_description')}</p>
+      </div>
+    </div>
+  )
 }
