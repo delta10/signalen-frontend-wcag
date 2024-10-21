5df2f99b6d878755459d5a35b738ae83334391df
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-10-21 16:52:27 +0200

    feat: add utrecht comp lib + add alert message (melding telefoon/opgeningstijden)

--- src/app/[locale]/incident/page.tsx
+++ src/app/[locale]/incident/page.tsx
@@ -1,5 +1,12 @@
 import { NextIntlClientProvider, useMessages, useTranslations } from 'next-intl'
 import { IncidentDescriptionForm } from '@/app/[locale]/incident/components/IncidentDescriptionForm'
+import {
+  Heading1,
+  Paragraph,
+  Alert,
+  Link,
+} from '@utrecht/component-library-react/dist/css-module'
+import '@utrecht/design-tokens/dist/index.css'
 
 export default async function Home() {
   return <IncidentDescriptionPage />
@@ -12,7 +19,20 @@ function IncidentDescriptionPage() {
   return (
     <>
       <div className="flex flex-col gap-4">
-        <h1>{t('heading')}</h1>
+        <Heading1>{t('heading')}</Heading1>
+        <Alert>
+          <div className="utrecht-alert__message" role="alert">
+            <Paragraph>
+              Lukt het niet om een melding te doen? Bel het telefoonnummer
+              <Link href="tel:14 020"> 14 020.</Link>
+            </Paragraph>
+            <Paragraph>
+              Wij zijn bereikbaar van maandag tot en met vrijdag van 08:00 tot
+              18:00 uur.
+            </Paragraph>
+          </div>
+        </Alert>
+
         <NextIntlClientProvider messages={messages}>
           <IncidentDescriptionForm />
         </NextIntlClientProvider>
