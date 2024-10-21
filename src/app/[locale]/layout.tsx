f22cc9965cf391a30283364f9eb9482b95425243
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-10-21 16:48:30 +0200

    feat: add utrecht document component + theme

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,6 +5,8 @@ import { Container } from '@/components'
 import { Header } from '@/app/[locale]/components/Header'
 import localFont from 'next/font/local'
 import AppProvider from '@/components/providers/AppProvider'
+import { Document } from '@utrecht/component-library-react/dist/css-module'
+import '@utrecht/design-tokens/dist/index.css'
 
 const font = localFont({
   src: '../../../public/fonts/open-sans.woff2',
@@ -25,10 +27,12 @@ export default function LocaleLayout({
     <html lang={locale} className={`${font.variable}`}>
       <body className="bg-gray-100">
         <AppProvider>
-          <Container className="bg-white">
-            <Header />
-            {children}
-          </Container>
+          <Document className="utrecht-theme">
+            <Container className="bg-white">
+              <Header />
+              {children}
+            </Container>
+          </Document>
         </AppProvider>
       </body>
     </html>
