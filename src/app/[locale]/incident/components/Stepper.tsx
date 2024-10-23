4c424b3ffc91450dea8f9d88537ef40a7d2eceef
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-10-23 16:09:29 +0200

    feat: swap: link,button,p comp to NLDS comp

--- src/app/[locale]/incident/components/Stepper.tsx
+++ src/app/[locale]/incident/components/Stepper.tsx
@@ -1,10 +1,14 @@
 'use client'
 
 import { useTranslations } from 'next-intl'
-import { Link, Paths, usePathname } from '@/routing/navigation'
+import { Paths, usePathname } from '@/routing/navigation'
 import { useSignalStore, useStepperStore } from '@/store/store'
 import { useEffect, useRef } from 'react'
-import { Button } from '@/components/ui/Button'
+import {
+  Button,
+  Link,
+  Paragraph,
+} from '@utrecht/component-library-react/dist/css-module'
 
 type StepperProps = {}
 
@@ -106,13 +110,13 @@ export const Stepper = ({}: StepperProps) => {
                 >
                   {index + 1}
                 </div>
-                <p
+                <Paragraph
                   className={`${
                     step == index + 1 ? 'text-xl font-semibold -ml-3' : ''
                   } md:block hidden transition duration-100 group-hover:underline group-focus:underline`}
                 >
                   {item.name}
-                </p>
+                </Paragraph>
               </Link>
             )
           })}
