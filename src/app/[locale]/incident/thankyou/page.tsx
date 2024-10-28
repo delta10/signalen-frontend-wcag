b5f7bdaaafcbba73ec8a5172ea1ce66221332fca
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-10-28 15:19:22 +0100

    refactor: swap h1 to heading component (with level={1})

--- src/app/[locale]/incident/thankyou/page.tsx
+++ src/app/[locale]/incident/thankyou/page.tsx
@@ -1,12 +1,12 @@
 import { useTranslations } from 'next-intl'
-import { Paragraph } from '@/components/index'
+import { Paragraph, Heading } from '@/components/index'
 
 export default function Thankyou() {
   const t = useTranslations('describe-thankyou')
 
   return (
     <div className="flex flex-col gap-8">
-      <h1>{t('heading')}</h1>
+      <Heading level={1}>{t('heading')}</Heading>
       <div className="flex flex-col gap-2">
         <Paragraph>{t('description_notification_number')}</Paragraph>
         <Paragraph>{t('description_notification_email')}</Paragraph>
