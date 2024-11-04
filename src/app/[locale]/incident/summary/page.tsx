b87e7cca75c4dc4d23091f8252a199114629918b
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2024-11-04 13:15:47 +0100

    feat: add metadata title to form steps + refactor current/max step

--- src/app/[locale]/incident/summary/page.tsx
+++ src/app/[locale]/incident/summary/page.tsx
@@ -1,6 +1,30 @@
 import { NextIntlClientProvider, useMessages, useTranslations } from 'next-intl'
 import { IncidentSummaryForm } from '@/app/[locale]/incident/summary/components/IncidentSummaryForm'
-import { HeadingGroup, Heading, PreHeading } from '@/components/index'
+import { Heading, HeadingGroup, PreHeading } from '@/components/index'
+import { getTranslations } from 'next-intl/server'
+import { createTitle } from '@/lib/utils/create-title'
+import { Metadata } from 'next/types'
+
+const currentStep = 4
+const maxStep = 4
+
+export async function generateMetadata(): Promise<Metadata> {
+  const errorMessage = ''
+  const t = await getTranslations('describe-summary')
+  const tGeneral = await getTranslations('general.describe_form')
+
+  return {
+    title: createTitle(
+      [
+        errorMessage ? tGeneral('title-prefix-error') : '',
+        tGeneral('pre-heading', { current: currentStep, max: maxStep }),
+        t('heading'),
+        'gemeente Voorbeeld',
+      ],
+      tGeneral('title-separator')
+    ),
+  }
+}
 
 export default function SummaryDetailsPage() {
   const t = useTranslations('describe-summary')
@@ -12,7 +36,7 @@ export default function SummaryDetailsPage() {
       <HeadingGroup>
         <Heading level={1}>{t('heading')}</Heading>
         <PreHeading>
-          {tGeneral('pre-heading', { current: 4, max: 4 })}
+          {tGeneral('pre-heading', { current: currentStep, max: maxStep })}
         </PreHeading>
       </HeadingGroup>
       <NextIntlClientProvider messages={messages}>
