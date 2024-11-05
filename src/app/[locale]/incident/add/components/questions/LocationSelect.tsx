c3a578c143514a2e62dc24701e6eae2ac111ff34
justian <justiandev@gmail.com>
2024-11-05 11:02:29 +0100

    Feat: show edit button for location select if location was selected

--- src/app/[locale]/incident/add/components/questions/LocationSelect.tsx
+++ src/app/[locale]/incident/add/components/questions/LocationSelect.tsx
@@ -5,6 +5,7 @@ import { MapProvider } from 'react-map-gl/maplibre'
 import { useFormContext } from 'react-hook-form'
 import React from 'react'
 import { Button, Paragraph } from '@/components/index'
+import { useFormStore } from '@/store/form_store'
 
 export interface LocationSelectProps {
   field?: PublicQuestion
@@ -15,6 +16,7 @@ export const LocationSelect = ({ field }: LocationSelectProps) => {
     formState: { errors },
   } = useFormContext()
   const errorMessage = errors['location']?.message as string
+  const { formState: formStoreState } = useFormStore()
 
   return (
     <div className="relative w-full">
@@ -31,21 +33,28 @@ export const LocationSelect = ({ field }: LocationSelectProps) => {
         {field ? field.meta.label : 'Waar is het?'}
       </label>
       <LocationMap />
+      <Paragraph>Locatie</Paragraph>
       <MapProvider>
         <MapDialog
           trigger={
-            <Button
-              id="location-button"
-              className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-none"
-              type="button"
-            >
-              Kies locatie
-            </Button>
+            formStoreState.coordinates[0] === 0 &&
+            formStoreState.coordinates[1] === 0 ? (
+              <Button
+                id="location-button"
+                className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 border-none"
+                type="button"
+              >
+                Kies locatie
+              </Button>
+            ) : (
+              <button id="location-button" type="button">
+                Wijzig locatie
+              </button>
+            )
           }
         />
       </MapProvider>
       {/* TODO: toon locatie, straatnaam bijv. */}
-      <Paragraph>Locatie</Paragraph>
     </div>
   )
 }
