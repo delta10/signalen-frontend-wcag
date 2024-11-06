080a1959169839812e7247cffe95f21b7d1cd697
justian <justiandev@gmail.com>
2024-11-06 15:39:42 +0100

    refactor: use NLDS components for contact form instead of radix components

--- src/app/[locale]/incident/contact/components/IncidentContactForm.tsx
+++ src/app/[locale]/incident/contact/components/IncidentContactForm.tsx
@@ -79,105 +79,53 @@ const IncidentContactForm = () => {
 
   return (
     <div>
-      <Form {...form}>
-        <div className="flex flex-col gap-4">
-          <Heading level={2}>{t('heading')}</Heading>
-          <Paragraph>{t('description')}</Paragraph>
-        </div>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-8 items-start"
-        >
-          <FormFieldTextbox
-            label={t('describe_phone_input_heading')}
-            autoComplete="phone"
-            invalid={invalidTODO}
-            description={tGeneral('not-required')}
-            errorMessage={errorMessageTODO}
-            {...form.register('phone')}
-          />
-          <FormField
-            name={'phone'}
-            control={form.control}
-            render={({ field, formState: { errors } }) => (
-              <FormItem error={errors.phone}>
-                <div>
-                  <FormLabel>{t('describe_phone_input_heading')}</FormLabel>
-                  <FormMessage />
-                </div>
-                <FormControl>
-                  <Input {...field} value={field.value!} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <FormFieldTextbox
-            label={t('describe_mail_input_heading')}
-            type="email"
-            autoComplete="email"
-            invalid={invalidTODO}
-            description={tGeneral('not-required')}
-            errorMessage={errorMessageTODO}
-            {...form.register('email')}
-          />
-          <FormField
-            name={'email'}
-            control={form.control}
-            render={({ field, formState: { errors } }) => (
-              <FormItem error={errors.email}>
-                <div>
-                  <FormLabel>{t('describe_mail_input_heading')}</FormLabel>
-                  <FormMessage />
-                </div>
-                <FormControl>
-                  <Input {...field} value={field.value!} />
-                </FormControl>
-              </FormItem>
-            )}
-          />
-          <Fieldset aria-describedby="todo-id">
-            <FieldsetLegend>
-              <Heading level={2}>{t('send_to_other_instance_heading')}</Heading>
-            </FieldsetLegend>
-            <div className="flex flex-col gap-4">
-              <FormFieldDescription id="todo-id">
-                <Paragraph>{t('send_to_other_instance_description')}</Paragraph>
-              </FormFieldDescription>
-              <div className="bg-gray-200 w-full p-4">
-                <FormFieldCheckbox
-                  label={t('describe_checkbox_input_description')}
-                  invalid={invalidTODO}
-                  errorMessage={errorMessageTODO}
-                  {...form.register('sharing_allowed')}
-                ></FormFieldCheckbox>
-                <FormField
-                  name={'sharing_allowed'}
-                  control={form.control}
-                  render={({ field, formState: { errors } }) => (
-                    <FormItem
-                      error={errors.sharing_allowed}
-                      className="flex flex-row gap-4"
-                    >
-                      <FormControl>
-                        <Checkbox
-                          {...field}
-                          checked={field.value}
-                          value={undefined}
-                          onCheckedChange={field.onChange}
-                        />
-                      </FormControl>
-                      <FormLabel className="font-normal">
-                        {t('describe_checkbox_input_description')}
-                      </FormLabel>
-                    </FormItem>
-                  )}
-                />
-              </div>
+      <div className="flex flex-col gap-4">
+        <Heading level={2}>{t('heading')}</Heading>
+        <Paragraph>{t('description')}</Paragraph>
+      </div>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-8 items-start"
+      >
+        <FormFieldTextbox
+          label={t('describe_phone_input_heading')}
+          autoComplete="phone"
+          errorMessage={form.formState.errors.phone?.message}
+          invalid={Boolean(form.formState.errors.phone?.message)}
+          description={tGeneral('not-required')}
+          {...form.register('phone')}
+        />
+        <FormFieldTextbox
+          label={t('describe_mail_input_heading')}
+          type="email"
+          autoComplete="email"
+          errorMessage={form.formState.errors.email?.message}
+          invalid={Boolean(form.formState.errors.email?.message)}
+          description={tGeneral('not-required')}
+          {...form.register('email')}
+        />
+        <Fieldset aria-describedby="todo-id">
+          <FieldsetLegend>
+            <Heading level={2}>{t('send_to_other_instance_heading')}</Heading>
+          </FieldsetLegend>
+          <div className="flex flex-col gap-4">
+            <FormFieldDescription id="todo-id">
+              <Paragraph>{t('send_to_other_instance_description')}</Paragraph>
+            </FormFieldDescription>
+            <div className="bg-gray-200 w-full p-4">
+              <FormFieldCheckbox
+                label={t('describe_checkbox_input_description')}
+                errorMessage={form.formState.errors.sharing_allowed?.message}
+                invalid={Boolean(
+                  form.formState.errors.sharing_allowed?.message
+                )}
+                {...form.register('sharing_allowed')}
+              ></FormFieldCheckbox>
             </div>
-          </Fieldset>
-          <IncidentFormFooter />
-        </form>
-      </Form>
+          </div>
+        </Fieldset>
+        <IncidentFormFooter />
+      </form>
     </div>
   )
 }
