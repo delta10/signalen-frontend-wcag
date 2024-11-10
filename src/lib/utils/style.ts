deca60e7fe3b2c0ddb156f0a95cb4a0ceb990eef
Robbert Broersma <git@robbertbroersma.nl>
2024-11-10 23:57:10 +0100

    chore: remove tailwind

--- src/lib/utils/style.ts
+++ src/lib/utils/style.ts
@@ -1,6 +1,5 @@
 import { ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
 
 export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs))
+  return clsx(inputs)
 }
