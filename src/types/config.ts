18c717d0d542314c7787d659bc6f67930a98bcb4
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2024-11-11 15:06:44 +0100

    ✨ update AppConfig type to include header and URL properties

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,8 +1,18 @@
 export type AppConfig = {
   base: {
     municipality: string
-    style: {
-      primaryColor: string
+    className?: string
+    URL: string
+    naam: string
+    header: {
+      logo: {
+        alt: string
+        url: string
+      }
+      title: string
+    }
+    style?: {
+      primaryColor?: string
     }
     map: {
       find_address_in_distance: number
