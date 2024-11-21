e6c002a48080d505f88d49a4bdac2e5517a25306
Yuri <yuri@delta10.nl>
2024-11-21 14:41:52 +0100

    feat: refactor form progress

--- src/app/[locale]/incident/contact/components/IncidentContactPage.tsx
+++ src/app/[locale]/incident/contact/components/IncidentContactPage.tsx
@@ -4,6 +4,7 @@ import { useTranslations } from 'next-intl'
 import { useFormStore } from '@/store/form_store'
 import { Heading, HeadingGroup, PreHeading } from '@/components'
 import { IncidentContactForm } from '@/app/[locale]/incident/contact/components/IncidentContactForm'
+import FormProgress from '@/app/[locale]/components/FormProgress'
 
 const currentStep = 3
 const maxStep = 4
@@ -15,13 +16,16 @@ export const IncidentContactPage = () => {
 
   if (loaded) {
     return (
-      <div className="flex flex-col gap-4">
-        <HeadingGroup>
-          <Heading level={1}>{t('heading')}</Heading>
-          <PreHeading>
-            {tGeneral('pre-heading', { current: currentStep, max: maxStep })}
-          </PreHeading>
-        </HeadingGroup>
+      <div className="flex flex-col gap-12">
+        <FormProgress>
+          <HeadingGroup>
+            <Heading level={1}>{t('heading')}</Heading>
+            <PreHeading>
+              {tGeneral('pre-heading', { current: currentStep, max: maxStep })}
+            </PreHeading>
+          </HeadingGroup>
+        </FormProgress>
+
         <IncidentContactForm />
       </div>
     )
