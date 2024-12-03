b9312d29b92b390c8d5b772b3f232410447062f5
justian <justiandev@gmail.com>
2024-12-03 09:53:08 +0100

    a11y: use correct html elements in <Alert> components

--- src/app/[locale]/incident/add/components/questions/PlainText.tsx
+++ src/app/[locale]/incident/add/components/questions/PlainText.tsx
@@ -1,13 +1,13 @@
 import { QuestionField } from '@/types/form'
-import Markdown from 'react-markdown'
 import { Alert } from '@/components'
+import { RenderMarkdown } from '@/components/ui/RenderMarkdown'
 
 interface PlainTextProps extends QuestionField {}
 
 export const PlainText = ({ field }: PlainTextProps) => {
   return field.meta.value ? (
     <Alert type="error" data-testid="plain-text-hard-stop">
-      <Markdown>{field.meta.value}</Markdown>
+      <RenderMarkdown text={field.meta.value} />
     </Alert>
   ) : null
 }
--- src/app/[locale]/incident/components/IncidentDescriptionPage.tsx
+++ src/app/[locale]/incident/components/IncidentDescriptionPage.tsx
@@ -2,12 +2,12 @@
 
 import { useTranslations } from 'next-intl'
 import { useFormStore } from '@/store/form_store'
-import { Heading, Paragraph, HeadingGroup, PreHeading } from '@/components'
-import { Alert, Link } from '@/components'
+import { Heading, HeadingGroup, PreHeading } from '@/components'
+import { Alert } from '@/components'
 import { IncidentDescriptionForm } from '@/app/[locale]/incident/components/IncidentDescriptionForm'
 import FormProgress from '@/app/[locale]/components/FormProgress'
 import { useConfig } from '@/hooks/useConfig'
-import Markdown, { defaultUrlTransform } from 'react-markdown'
+import { RenderMarkdown } from '@/components/ui/RenderMarkdown'
 
 const currentStep = 1
 const maxStep = 4
@@ -35,17 +35,7 @@ export const IncidentDescriptionPage = () => {
           </FormProgress>
           {config ? (
             <Alert>
-              <Markdown
-                urlTransform={(url) =>
-                  url.startsWith('tel:') ? url : defaultUrlTransform(url)
-                }
-                components={{
-                  p: (props) => <Paragraph>{props.children}</Paragraph>,
-                  a: (props) => <Link {...props}>{props.children}</Link>,
-                }}
-              >
-                {t('alert.help_text')}
-              </Markdown>
+              <RenderMarkdown text={t('alert.help_text')} />
             </Alert>
           ) : null}
           <IncidentDescriptionForm />
