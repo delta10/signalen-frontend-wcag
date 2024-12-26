47402cf42634e387e589f1f0416d8ddd2ab66eb2
Robbert Broersma <git@robbertbroersma.nl>
2024-12-26 22:33:25 +0100

    feat: add ParagraphOrList component for selected entries

---
+++ src/components/ui/ParagraphOrList.tsx
@@ -0,0 +1,22 @@
+import { Key, ReactNode } from 'react'
+import { Paragraph, UnorderedList, UnorderedListItem } from '..'
+
+export type ParagraphOrListEntry = [Key, ReactNode]
+
+export interface ParagraphOrListProps {
+  entries: ParagraphOrListEntry[]
+}
+
+export const ParagraphOrList = ({ entries }: ParagraphOrListProps) => {
+  if (entries.length >= 2) {
+    return (
+      <UnorderedList>
+        {entries.map(([id, item], index) => (
+          <UnorderedListItem key={index}>{item}</UnorderedListItem>
+        ))}
+      </UnorderedList>
+    )
+  } else if (entries.length === 1) {
+    return <Paragraph>{entries[0][1]}</Paragraph>
+  }
+}
