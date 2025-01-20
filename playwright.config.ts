f7cf1226dd9fa5a5bba4a0e3c1341631cd4a299c
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2025-01-20 11:59:03 +0100

    test: increase timeout CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  timeout: 3000,
+  /* Shorter test time for dev */
+  timeout: process.env.CI ? 30000 : 15000,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
