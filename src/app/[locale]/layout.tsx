56ce94e4f943c568f0e36aee8d45cd5eb6386bf3
Yuri <yuri@delta10.nl>
2025-03-27 10:51:10 +0100

    feat: refactor layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,18 +1,12 @@
 import { notFound } from 'next/navigation'
 import React from 'react'
 import { getAllAvailableLocales } from '@/lib/utils/locale'
-import { Root, PageLayout, Body, PageBody, Article } from '@/components'
-import { Header } from '@/app/[locale]/components/Header'
+import { Root, Body } from '@/components'
 import localFont from 'next/font/local'
-import { NextIntlClientProvider, useMessages } from 'next-intl'
-import { Footer } from '@/app/[locale]/components/Footer'
-import pick from 'lodash/pick'
 import type { PropsWithChildren } from 'react'
 import { getServerConfig } from '@/services/config/config'
 import { AppConfig } from '@/types/config'
 import { ConfigProvider } from '@/contexts/ConfigContext'
-import { headers } from 'next/headers'
-import clsx from 'clsx'
 
 const font = localFont({
   src: '../../../public/fonts/open-sans.woff2',
@@ -28,11 +22,6 @@ const LocaleLayout = ({
   config: AppConfig
   params: { locale: any }
 }>) => {
-  const messages = useMessages()
-  const headersList = headers()
-  const referer = headersList.get('referer') || ''
-  const isFromIncidentMap = referer.includes('incident-map')
-
   if (!getAllAvailableLocales().includes(locale as any)) notFound()
 
   return (
@@ -41,30 +30,7 @@ const LocaleLayout = ({
       className={`${font.variable} purmerend-theme purmerend-theme--media-query`}
     >
       <ConfigProvider config={config}>
-        <Body>
-          <PageLayout>
-            <NextIntlClientProvider
-              messages={pick(messages, 'current_organisation')}
-            >
-              <Header />
-            </NextIntlClientProvider>
-
-            <PageBody className={clsx(isFromIncidentMap ? 'incident-map' : '')}>
-              {!isFromIncidentMap ? (
-                <Article className="max-w-3xl mx-auto px-4 lg:px-0">
-                  {children}
-                </Article>
-              ) : (
-                <>{children}</>
-              )}
-            </PageBody>
-            {!isFromIncidentMap && (
-              <NextIntlClientProvider messages={pick(messages, 'footer')}>
-                <Footer />
-              </NextIntlClientProvider>
-            )}
-          </PageLayout>
-        </Body>
+        <Body>{children}</Body>
       </ConfigProvider>
     </Root>
   )
