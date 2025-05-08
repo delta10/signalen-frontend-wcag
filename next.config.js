fe75abb3d5b53764d2926baa0d8a573cfa270511
georgealpha9 <joris@delta10.nl>
2025-05-08 13:04:04 +0200

    WIP Exclude manage from routing attempt 2

--- next.config.js
+++ next.config.js
@@ -3,13 +3,4 @@ const withNextIntl = require('next-intl/plugin')()
 
 module.exports = withNextIntl({
   output: 'standalone',
-  async redirects() {
-    return [
-      {
-        source: '/manage',
-        destination: '/manage', // Redirect to a 404 page or any other page
-        permanent: false, // Set to true if this is a permanent redirect
-      },
-    ]
-  },
 })
