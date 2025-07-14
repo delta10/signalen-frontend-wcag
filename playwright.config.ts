5e44f9235ba8394edf5cd9b09b8ebfc38a1e102f
Yuri <yuri@delta10.nl>
2025-07-14 13:04:24 +0200

    fix: e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,7 +57,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: process.env.CI ? 'node .next/standalone/server.js' : 'npm run dev',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
   },
