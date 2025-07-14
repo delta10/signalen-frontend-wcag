0292097b3e764cdc52bcb820f8a1eff49292f79d
Yuri <yuri@delta10.nl>
2025-07-14 12:58:01 +0200

    chore: remove cps chore: remove font import

--- next.config.js
+++ next.config.js
@@ -1,55 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require('next-intl/plugin')()
 
-// Define Content Security Policy
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' ${
-    process.env.NODE_ENV === 'development' ? 'unsafe-eval' : ''
-  };
-  worker-src 'self' blob:;
-  style-src 'self' 'unsafe-inline';
-  font-src 'self' https://fonts.gstatic.com data:;
-  img-src 'self' data: blob: https:;
-  connect-src 'self' https: wss: ws:;
-  frame-src 'self';
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'self';
-  block-all-mixed-content;
-  upgrade-insecure-requests;
-`
-
-// Define security headers
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin',
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=(self)',
-  },
-]
-
 module.exports = withNextIntl({
   output: 'standalone',
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes
-        source: '/:path*',
-        headers: securityHeaders,
-      },
-    ]
-  },
 })
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,6 @@ import type { Metadata } from 'next'
 import './globals.css'
 import './nl-design-system-overrides.css'
 
-// todo how to do font
-import '@nl-design-system-community/purmerend-design-tokens/src/font.js'
-
 export const metadata: Metadata = {
   description: 'Maak een melding bij jouw gemeente.',
 }
