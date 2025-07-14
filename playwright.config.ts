83f664a32e89ce62898e090473ff1ae155ddb326
Yuri <yuri@delta10.nl>
2025-07-14 13:45:36 +0200

    fix: nu dan?

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: process.env.CI ? 'on-first-retry' : 'on',
+    baseURL: 'http://localhost:3000',
   },
 
   /* Configure projects for major browsers */
@@ -57,8 +58,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    port: 3000,
+    command: 'npm run start:test-e2e',
+    url: 'http://localhost:3000',
+    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 })
