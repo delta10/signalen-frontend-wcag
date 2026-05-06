c64e245097f8f76e5721f103519d1ddee75e40ab
Vienna Meijer <viennameijer@gmail.com>
2026-05-06 12:27:46 +0200

    feat: make app metadata description use organization name from config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from 'next'
 import '@nl-design-system-candidate/button-css/button.css'
 import './app.css'
+import config from '../../config.json'
 
 export const metadata: Metadata = {
-  description: 'Maak een melding bij de provincie.',
+  description: config.base.naam
+    ? `Maak een melding bij ${config.base.naam}.`
+    : 'Maak een melding.',
 }
 
 export default function RootLayout({
