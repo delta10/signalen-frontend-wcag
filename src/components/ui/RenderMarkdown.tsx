dae063304560a196531bf6b768ea29dc28458e1f
Vienna Meijer <viennameijer@gmail.com>
2026-09-10 12:49:00 +0200

    feat: customize noord-brabant content and improve markdown rendering

--- src/components/ui/RenderMarkdown.tsx
+++ src/components/ui/RenderMarkdown.tsx
@@ -1,18 +1,38 @@
 import Markdown, { defaultUrlTransform } from 'react-markdown'
-import { Link, Paragraph } from '@/components'
+import {
+  HTMLContent,
+  Link,
+  Paragraph,
+  UnorderedList,
+  UnorderedListItem,
+} from '@/components'
 
 export const RenderMarkdown = ({ text }: { text: string }) => {
   return (
-    <Markdown
-      urlTransform={(url) =>
-        url.startsWith('tel:') ? url : defaultUrlTransform(url)
-      }
-      components={{
-        p: (props) => <Paragraph>{props.children}</Paragraph>,
-        a: (props) => <Link {...props}>{props.children}</Link>,
-      }}
-    >
-      {text}
-    </Markdown>
+    <HTMLContent className="flex flex-col gap-[1em]">
+      <Markdown
+        urlTransform={(url) =>
+          url.startsWith('tel:') ? url : defaultUrlTransform(url)
+        }
+        components={{
+          p: (props) => <Paragraph>{props.children}</Paragraph>,
+          a: ({ href, children, title }) => (
+            <Link href={href} title={title}>
+              {children}
+            </Link>
+          ),
+          ul: (props) => (
+            <UnorderedList className="!list-disc">
+              {props.children}
+            </UnorderedList>
+          ),
+          li: (props) => (
+            <UnorderedListItem>{props.children}</UnorderedListItem>
+          ),
+        }}
+      >
+        {text}
+      </Markdown>
+    </HTMLContent>
   )
 }
